1a33f89e2bc484163e637ed13a9cf6467fc9d0de
Arif Zakuan <arif.zakuan@gmail.com>
2023-07-10 11:48:09 +0800

    Updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,30 +6,34 @@
 <svelte:head>
   <title>❤️ Arif Zakuan</title>
   <meta property="og:title" content="❤️ Arif Zakuan" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </svelte:head>
 
 <div class="relative">
-<Navbar fluid="true" navClass="px-2 sm:px-4 py-2.5 fixed  bg-white opacity-80 w-full z-20 top-0 left-0 border-b" color='dark' let:hidden let:toggle>
+<Navbar fluid="true" navClass="z-40 px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b " color='light' let:hidden let:toggle>
     <NavBrand href="/">
       <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
         Zakuan
       </span>
     </NavBrand>
-    <NavHamburger on:click={toggle}/>
+    
+    <NavHamburger on:click={toggle} />
     <NavUl {hidden} activeClass="text-red" class="content-center">
-      <NavLi href="#about" class="font-medium">About me</NavLi>
-      <NavLi href="#projects" class="font-medium">Projects</NavLi>
-      <NavLi href="#contact" class="font-medium">Contact</NavLi>
+      <NavLi href="#about" class="font-medium dark:text-white">About me</NavLi>
+      <NavLi href="#projects" class="font-medium dark:text-white">Projects</NavLi>
+      <NavLi href="#contact" class="font-medium dark:text-white">Contact</NavLi>
     </NavUl>
-    <DarkMode class="text-sm"/> 
+    <div class="rounded-lg w-auto h-auto bg-slate-100 dark:bg-slate-700">
+      <DarkMode class="text-sm hover:bg-transparent"/>
+    </div>
   </Navbar>
   
-<div class="py-32 text-gray-900">
+<div class="py-32 text-gray-900 ">
 <h1 class="lg:text-5xl text-3xl text-center font-sans py-5">Arif Zakuan</h1>
 <p class="text-2xl text-center font-sans px-10"> I'm an aspiring software developer building stuff for mobile and the web</p>
 </div>
 <div class="" id="about">
-    <div class=" bg-slate-100 p-20 lg:px-36 space-y-5 lg:flex max-sm:px-8">
+    <div class=" bg-slate-100 dark:bg-gray-800 p-20 lg:px-36 space-y-5 lg:flex max-sm:px-8">
       <img src="assets/profile-pic.jpeg" alt="" class="lg:w-80 lg:h-h-80"/>
       <div>
         <h1 class="text-center lg:text-left font-sans text-gray-900 text-2xl font-bold py-5 lg:px-20">A bit about me!</h1>
@@ -72,9 +76,9 @@
       </Card>
       <Card href="https://github.com/tehcanai/SimpleLogin-App" img="assets/login.png" >
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">SimpleLogin</h5>
-        <p class="font-bold text-gray-800 dark:text-gray-800 leading-normal text-left py-2">
+        <p class="font-bold text-gray-800 leading-normal text-left py-2 dark:text-white ">
          Flutter · Laravel · Postgresql
-        <p class="font-normal text-gray-700 dark:text-gray-700 leading-normal text-left ">
+        <p class="font-normal text-gray-700 leading-normal text-left dark:text-white  ">
           SimpleLogin is a simple login app that uses Laravel as the backend. The app allows users to register and authenticates users via Passport when logging in.
         </p>
       </Card>
@@ -93,5 +97,6 @@
   </div>
 </div>
 </div>
+
 
 
